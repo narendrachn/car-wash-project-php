f7122c3870b968e3cd0a1b189f61f1d69d5d3dac
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-18 11:38:48 -0700

    Add login form page code to file

--- index.php
+++ index.php
@@ -5,10 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car Wash</title>
+
+    <link rel="stylesheet" href="styles.css">
+    
 </head>
 <body>
     <main>
         <header>Car Wash</header>
+        <div class="login-container">
+        <h2>Login</h2>
+        <form method="post" action="login_process.php">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login">
+        </form>
+    </div>
     </main>
 </body>
 </html>
