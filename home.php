d60e5da1844a51b6650673ce8416f39aacfe6acc
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-19 20:39:40 -0700

    Add more page links in navigation bar

--- home.php
+++ home.php
@@ -10,16 +10,17 @@
     <nav>
         <ul class="menu">
             <li><a href="#">Home</a></li>
-            <li><a href="#">Book Appointment</a></li>
-            <li><a href="#">About Us</a></li>
+            <li><a href="appointments.php">Appointments</a></li>
+            <li><a href="payment.php">Payment</a></li>
+            <li><a href="status.php">Status</a></li>
         </ul>
     </nav>
     <main>
         <header>Car Wash</header>
         <div class="Home">
-            <button>Book Appointment</button>
-            <button>Change Appointment</button>
-            <button>Check availability</button>
+            <button><a href="appointments.php">Book Appointment</a></button>
+            <button><a href="#">Change Appointment</a></button>
+            <button><a href="#">Check availability</a></button>
             
         </div>
     </main>
