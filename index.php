b8ea0cb2255d7b53533bce5d7498af63b05c1c29
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-19 20:41:57 -0700

    Add code for styles to the navigation bar

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
     <nav>
         <ul class="menu">
             <li><a href="home.php">Home</a></li>
-            <li><a href="#">Book Appointment</a></li>
-            <li><a href="#">About Us</a></li>
+            <li><a href="appointments.php">Appointments</a></li>
+            <li><a href="payment.php">Payment</a></li>
+            <li><a href="status.php">Status</a></li>
         </ul>
     </nav>
     <main>
