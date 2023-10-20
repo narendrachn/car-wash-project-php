a5808a07494d8a6a1f1bebbcbde23555c9ef0bc2
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-19 20:40:12 -0700

    Add status page to the repository

---
+++ status.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Status</title>
+</head>
+<body>
+    <nav>
+        <ul class="menu">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="appointments.php">Appointments</a></li>
+            <li><a href="payment.php">Payment</a></li>
+            <li><a href="#">Status</a></li>
+        </ul>
+    </nav>
+    
+</body>
+</html>
