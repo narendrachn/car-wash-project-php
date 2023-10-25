b1931753ec1731d3274bc65a1abf8c718172c485
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-25 12:01:32 -0700

    Create global variable with sample attributes in it

--- update_profile.php
+++ update_profile.php
@@ -1,6 +1,23 @@
 <?php
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $contact = $_POST["contact"];
 
+        // TODO: Perform input validation and security checks
+
+        // Update the user's profile in the database (example: MySQL)
+        // Replace with your actual database connection and SQL query
+        $servername = "your_servername";
+        $username = "your_username";
+        $password = "your_password";
+        $dbname = "your_database";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        
+    }
 
 
 ?>
