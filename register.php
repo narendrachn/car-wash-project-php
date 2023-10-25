4b69b57d27c4de3541dabb5149b814cffe33979f
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-10-25 11:55:54 -0700

    Add code to access database for storage

--- register.php
+++ register.php
@@ -10,8 +10,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = "your_username";
     $password = "your_password";
     $dbname = "your_database";
-    
+
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 }
 ?>
