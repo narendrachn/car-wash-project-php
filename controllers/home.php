36b82add781526c28ad8d07b051428bd52c94526
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-11-08 00:44:58 -0800

    Update nav link for specific links

--- controllers/home.php
+++ controllers/home.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+<?php 
 
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <nav>
-        <ul class="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="appointments.php">Appointments</a></li>
-            <li><a href="payment.php">Payment</a></li>
-            <li><a href="status.php">Status</a></li>
-        </ul>
-    </nav>
-    <main>
-        <header>Car Wash</header>
-        <div class="Home">
-            <button><a href="appointments.php">Book Appointment</a></button>
-            <button><a href="#">Change Appointment</a></button>
-            <button><a href="#">Check availability</a></button>
-            
-        </div>
-    </main>
-    
-</body>
-</html>
+  require "../views/home.view.php";
+
+
+?>
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,9 +2,8 @@
     <ul class="menu">
         <li><a href="home.php">Home</a></li>
         <li><a href="appointments.php">Appointments</a></li>
-        <li><a href="payment.php">Payment</a></li>
+        <li><a href="profile.php">Profile</a></li>
         <li><a href="status.php">Status</a></li>
-        <li><a href="register.html">register</a></li>
         <li><a href="profile.html">Contact Us</a></li>
     </ul>
 </nav>
