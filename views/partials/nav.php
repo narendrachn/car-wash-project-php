fd86e59846be91b6fd79ed0493e2898a926a334b
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-11-07 21:06:29 -0800

    Add nav contents to nav partial file

---
+++ views/partials/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul class="menu">
+        <li><a href="home.php">Home</a></li>
+        <li><a href="appointments.php">Appointments</a></li>
+        <li><a href="payment.php">Payment</a></li>
+        <li><a href="status.php">Status</a></li>
+        <li><a href="register.html">register</a></li>
+        <li><a href="profile.html">Contact Us</a></li>
+    </ul>
+</nav>
