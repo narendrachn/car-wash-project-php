70ab835071840682ebfb4b628615a8f769b1b610
narendrachn <109662555+narendrachn@users.noreply.github.com>
2023-11-29 02:58:14 -0800

    replace code with require tags

--- controllers/payment.php
+++ controllers/payment.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Make a Payments</title>
-    
-    <link rel="stylesheet" href="styles.css">
-
-</head>
-<body>
-    <nav>
-        <ul class="menu">
-            <li><a href="home.php">Home</a></li>
-            <li><a href="#">Appointment</a></li>
-            <li><a href="#">About Us</a></li>
-        </ul>
-    </nav>
-    <main>
+<?php
+require "../views/partials/head.php";
+require "../views/partials/nav.php";
+?>
+<main>
         <h2>Payment Information</h2>
-        <form action="" method="POST">
+        <form action="status.php" method="POST">
             <label for="name">Name:</label>
             <input type="text" id="name" name="name"><br>
             <label for="email">Email:</label>
@@ -34,9 +19,8 @@
                 <option value="150">$150</option>
                 <option value="200">$200</option>
             </select><br>
-            <button type="submit"><a href="payment.php">Submit</a></button>
+            <button type="submit"><a href="status.php">Submit</a></button>
         </form>
 
     </main>
 </body>
-</html>
--- controllers/status.php
+++ controllers/status.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Status</title>
+<?php
+require "../views/partials/head.php";
+require "../views/partials/nav.php";
+?>
 
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <nav>
-        <ul class="menu">
-            <li><a href="home.php">Home</a></li>
-            <li><a href="appointments.php">Appointments</a></li>
-            <li><a href="payment.php">Payment</a></li>
-            <li><a href="#">Status</a></li>
-        </ul>
-    </nav>
-    
-</body>
-</html>
+<main>
+    <h3>Booking success</h3>
+</main>
